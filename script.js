3a207b28731f0e5032d20634e6f8836ec738cd8f
Arash Hosseini <arash.hosseini3@gmail.com>
2020-04-25 19:53:35 -0400

    CSS changes and bug fixes

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function openLinkedIn(link) {
     window.open(link);
 }
+
+function openlink(link) {
+    window.open(link);
+}
+// window.open(location.origin + "/compleccity/pdf-files/" + pdfName)
